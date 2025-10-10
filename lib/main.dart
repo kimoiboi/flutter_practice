11a54092a9d26d5d80d91c1bf933b58c279be0ee
kimoiboi <elgendik3@gmail.com>
2025-10-10 12:29:26 -0400

    Added clear comments to steps taken
    Documented each widget used and edited Slider a little bit

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,16 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  double _currentSliderVal = 0;
+  double _currentSliderVal = 0; // Starting Value of Slider widget
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
+      home: Scaffold( // This creates the skeleton of app (White page)), //Scaffold
+        appBar: AppBar( // The AppBar holding the 911 button & CPR title
           title: const Text('CPR Mock-Up'),
           backgroundColor: Colors.blueAccent,
-          actions: <Widget>[
+          actions: <Widget>[ // Creates the button and gives instructions when pressed
             ElevatedButton(
               onPressed: () {
                 print('Calling 911...');
@@ -31,13 +31,13 @@ class _MyAppState extends State<MyApp> {
             ),
           ],
         ),
-        body: Center(
-          child: Slider(
-            value: _currentSliderVal,
-            min: 0,
-            max: 5,
+        body: Center( // Centers whatever widget built to the center Body
+          child: Slider( // Creates a child of the widget Slider
+            value: _currentSliderVal, // Initialize the value
+            min: 0, // Slider Number starts at 0
+            max: 5, // Max Slider Number ends at 5
             divisions: 5,
-            label: _currentSliderVal.round().toString(),
+            label: _currentSliderVal.round().toString(), // Instead of using double values and cleaner transition of steps
             onChanged: (double value) {
               setState(() {
                 _currentSliderVal = value;
