a663744fa379e1fca327bb1010b61f0e83d76b6d
kimoiboi <elgendik3@gmail.com>
2025-10-10 12:21:44 -0400

    Added Slider
    Created a generic slider, will pack the widget in a Stack and hopefully utilize the Image Assets widget to correspond to the changing slider(for different instructions)

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double _currentSliderVal = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(  //This creates the skeleton of app (White page)), //Scaffold
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('CPR Mock-Up'), //Creates a text.
+          title: const Text('CPR Mock-Up'),
           backgroundColor: Colors.blueAccent,
           actions: <Widget>[
             ElevatedButton(
-                onPressed: (){
-                  print('Calling 911...');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                ),
-                child: Text(
-                  '911',
-                  style: TextStyle(
-                    color: Colors.white70
-                )
-            )
-        )],
-        ), //AppBar
-      ) //Scaffold
-    ); //MaterialApp
+              onPressed: () {
+                print('Calling 911...');
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text('911', style: TextStyle(color: Colors.white70)),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Slider(
+            value: _currentSliderVal,
+            min: 0,
+            max: 5,
+            divisions: 5,
+            label: _currentSliderVal.round().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderVal = value;
+              });
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
-
